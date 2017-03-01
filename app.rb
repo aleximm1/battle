fd34990d8ab5c6e7755a26667683ed40f559b6a3
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-03-01 16:15:21 +0000

    record hit points for player 2 passed test

--- app.rb
+++ app.rb
@@ -3,17 +3,35 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   get '/' do
     'Hello Battle!'
+    "player1 = " << session[:player1].inspect
+    "player2 = " << session[:player2].inspect
   end
   run if app_file == $0
+
+  enable :sessions
 
   get '/battle/add_players' do
     erb(:add_names)
   end
 
-  post '/battle/show_players' do
+  # get '/:player1' do
+  #   session['player1'] = params['player1']
+  # end
+  #
+  # get '/:player2' do
+  #   session['player2'] = params['player2']
+  # end
+
+  post '/battle/set_players' do
     p params
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session['player1'] = params[:player1]
+    session['player2'] = params[:player2]
+    redirect '/battle/show_players'
+  end
+
+  get '/battle/show_players' do
+    @player1 = session['player1']
+    @player2 = session['player2']
     erb(:show_names)
   end
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 # So we can play a personalised game of Battle,
 # We want to Start a fight by entering our names and seeing them
 # Rspec.feature "User Stories", :type => :feature do
-RSpec.feature 'allows name entry' do
+RSpec.feature 'initializing battle game with 2 players' do
   scenario 'allows players to enter their names and see them' do
     visit "/battle/add_players"
     fill_in "player1", :with => "Noora"
@@ -12,5 +12,16 @@ RSpec.feature 'allows name entry' do
     click_button "Submit"
     expect(page).to have_text("Player 1: Noora, Player 2: Alex")
   end
-
+end
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+  RSpec.feature 'recording hit points' do
+    scenario 'shows player 2\'s hit points' do
+      visit "/battle/add_players"
+      fill_in "player1", :with => "Noora"
+      fill_in "player2", :with => "Alex"
+      click_button "Submit"
+      expect(page).to have_content 'Alex: 100HP'
+    end
 end
